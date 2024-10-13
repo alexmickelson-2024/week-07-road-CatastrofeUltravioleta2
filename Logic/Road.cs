007d4c2e3175ae2b1b9f7826ae196c6f8339ec18
Wilhelm Gonce <goncequintanawilhelm@gmail.com>
2024-10-12 18:20:31 -0600

    code

---
+++ Logic/Road.cs
@@ -0,0 +1,20 @@
+public class Road : IRoad
+{
+    private IVehicle[,] vehicles;
+    public int RowCount { get; }
+    public int ColCount { get; }
+    public Road(int row, int col)
+    {
+        RowCount = row;
+        ColCount = col;
+        vehicles = new IVehicle[RowCount,ColCount];
+    }
+    public void SetVehicle(int row, int column, IVehicle vehicle)
+    {
+        vehicles[row,column] = vehicle;
+    }
+    public IVehicle? GetVehicle(int row, int column)
+    {
+        return vehicles[row,column];
+    }
+}
